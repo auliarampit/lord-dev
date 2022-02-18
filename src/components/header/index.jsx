12c96e4f8e95a39395c01d6d3828d6745291e46f
auliarahmat <auliarahmat25599@gmail.com>
2022-02-18 17:56:16 +0700

    sample project and add lottie

---
+++ src/components/header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.css'
+
+function HeaderComponent(props) {
+  return (
+    <div>HeaderComponent</div>
+  )
+}
+
+HeaderComponent.propTypes = {}
+
+export default HeaderComponent
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './styles.css'
+import DataNotFound from '../../assets/lotties/no-data.json'
+import Lottie from 'lottie-react';
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='container'>
+      <Lottie animationData={DataNotFound} loop={true} />
+    </div>
   )
 }
