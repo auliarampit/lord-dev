90516ddd2d69b8b44c1906790fab1da651e2dae9
auliarahmat <auliarahmat25599@gmail.com>
2022-02-21 23:19:29 +0700

    example navbar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
 import './styles.css'
-
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 function HeaderComponent(props) {
   return (
-    <div>HeaderComponent</div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" style={{background: 'white'}}>
+        <Toolbar>
+          <Typography variant="h6" component="div" color={'InfoText'} sx={{ flexGrow: 1 }}>
+            LorDev
+          </Typography>
+          <Button color="info">Home</Button>
+          <Button color="info">Our Project</Button>
+          <Button color="info">Service</Button>
+          <Button color="info">FeedBack</Button>
+          <Button color="info">FAQ</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './styles.css'
 import DataNotFound from '../../assets/lotties/no-data.json'
 import Lottie from 'lottie-react';
+import { HeaderComponent } from '../../components';
 
 export default function HomePage() {
   return (
     <div className='container'>
-      <Lottie animationData={DataNotFound} loop={true} />
+      <HeaderComponent />
+      {/* <Lottie animationData={DataNotFound} loop={true} /> */}
     </div>
   )
 }
